cdc86e824e622442365bde156599b4f29d903e7d
AnujShr <30742608+AnujShr@users.noreply.github.com>
2018-11-14 00:26:59 +0545

    add admin style and script and blade

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>blueSky</title>
+    <link href="{{asset('css/admin/app.css')}}" rel="stylesheet">
+</head>
+
+<body class="nav-md">
+<div class="container body">
+    <div class="main_container">
+        @include('admin.partials.sidebar')
+        @include('admin.partials.top-navigation')
+        @yield('content')
+        @include('admin.partials.footer')
+    </div>
+</div>
+</body>
+
+<script src="{{asset('js/admin/app.js')}}"></script>
+
+{{--@endsection--}}
+
+</html>
