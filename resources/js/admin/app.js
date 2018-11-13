a8f8a7eb0a7b4e4061c35e60ca146ec6e3183ee4
AnujShr <30742608+AnujShr@users.noreply.github.com>
2018-11-14 00:26:59 +0545

    add admin style and script and blade

---
+++ resources/js/admin/app.js
@@ -0,0 +1,18 @@
+window.moment = require('moment');
+require('../bootstrap');
+
+$(function(){
+    require('../../gentellea/fastclick/lib/fastclick.js');
+    require('../../gentellea/nprogress/nprogress.js');
+    require('../../gentellea/Chart.js/dist/Chart.min.js');
+    require('../../gentellea/gauge.js/dist/gauge.min.js');
+    require('../../gentellea/nprogress/nprogress.js');
+    require('../../gentellea/bootstrap-progressbar/bootstrap-progressbar.min.js');
+    require('../../gentellea/iCheck/icheck.min.js');
+    require('../../gentellea/skycons/skycons.js');
+    require('../../gentellea/DateJS/build/date.js');
+    require('../../gentellea/bootstrap-daterangepicker/daterangepicker.js');
+    require('../../gentellea/bootstrap-wysiwyg/js/bootstrap-wysiwyg.min.js');
+    require('../../gentellea/google-code-prettify/src/prettify.js');
+    require('../../gentellea/js/custom.min.js');
+});
