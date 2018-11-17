f2c7bce385bcbc8a9806f2f6043ffaeb08b77a28
AnujShr <30742608+AnujShr@users.noreply.github.com>
2018-11-17 15:59:31 +0545

    add contact blade
    (cherry picked from commit 347d195d75a3415045207e1311d6a33f184ced2f)

---
+++ app/Http/Controllers/HelpcenterController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\helpcenter;
+use Illuminate\Http\Request;
+
+class HelpcenterController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\helpcenter  $helpcenter
+     * @return \Illuminate\Http\Response
+     */
+    public function show(helpcenter $helpcenter)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\helpcenter  $helpcenter
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(helpcenter $helpcenter)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\helpcenter  $helpcenter
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, helpcenter $helpcenter)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\helpcenter  $helpcenter
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(helpcenter $helpcenter)
+    {
+        //
+    }
+}
---
+++ app/Role.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Role extends Model
+{
+    protected $fillable = ['user_id', 'role_id'];
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class);
+    }
+}
