f54e4e1f94d9dcc8a66fea5eb2d8cdb4aa0b1e91
AnujShr <30742608+AnujShr@users.noreply.github.com>
2018-11-23 22:14:19 +0545

    new login form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,68 +1,148 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-    <link rel="stylesheet" type="text/css" href="{{asset('css/login.css')}}"/>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap Elegant Account Login Form with Avatar Icon</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,400i,500,600,700,800,900" rel="stylesheet">
+    {{--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">--}}
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <style type="text/css">
+        .class {
+            width: 100%;
+            height: 100%;
+            position: absolute;
+            z-index: -5;
+            filter: brightness(85%);
+        }
 
+
+        body {
+            color: #999;
+            background: #f5f5f5;
+            font-family: 'Montserrat', sans-serif;
+        }
+
+        .form-control {
+            box-shadow: none;
+            border-color: #ddd;
+        }
+
+        .form-control:focus {
+            border-color: #4aba70;
+        }
+
+        .login-form {
+            width: 350px;
+            margin: 0 auto;
+            padding: 30px 0;
+        }
+
+        .login-form form {
+            color: #434343;
+            border-radius: 1px;
+            margin-bottom: 15px;
+            background: #FFFFE0;
+            border: 1px solid #f3f3f3;
+            box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
+            padding: 30px;
+        }
+
+        .login-form h4 {
+            text-align: center;
+            font-size: 22px;
+            margin-bottom: 20px;
+        }
+
+        .login-form .avatar {
+            color: #fff;
+            margin: 0 auto 30px;
+            text-align: center;
+            width: 100px;
+            height: 100px;
+            border-radius: 50%;
+            z-index: 9;
+            background: #007bff;
+            padding: 15px;
+            box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.1);
+        }
+
+        .login-form .avatar i {
+            font-size: 62px;
+        }
+
+        .login-form .form-group {
+            margin-bottom: 20px;
+        }
+
+        .login-form .form-control, .login-form .btn {
+            min-height: 40px;
+            border-radius: 2px;
+            transition: all 0.5s;
+        }
+
+        .login-form .close {
+            position: absolute;
+            top: 15px;
+            right: 15px;
+        }
+
+        .login-form .btn {
+            background: #007bff;
+            border: none;
+            line-height: normal;
+            color: #ffffff;
+        }
+
+        .login-form .btn:hover, .login-form .btn:focus {
+            background: #42ae68;
+        }
+
+        .login-form .checkbox-inline {
+            float: left;
+        }
+
+        .login-form input[type="checkbox"] {
+            margin-top: 2px;
+        }
+
+        .login-form .forgot-link {
+            float: right;
+        }
+
+        .login-form .small {
+            font-size: 13px;
+        }
+
+        .login-form a {
+            color: #4aba70;
+        }
+    </style>
 </head>
 <body>
-<p class="tip">Click on button in image container</p>
-<div class="cont">
-    <div class="form sign-in">
-        <h2>Welcome back,</h2>
-        <form action="{{route('login')}}" method='post'>
-            {{
-            csrf_field()
-            }} <label>
-                <span>Email</span>
-                <input name="email" type="email"/>
-            </label>
-            <label>
-                <span>Password</span>
-                <input name="password" type="password"/>
-            </label>
-            <p class="forgot-pass">Forgot password?</p>
-            <button type="submit" class="submit">Sign In</button>
-            <button type="button" class="fb-btn">Connect with <span>facebook</span></button>
-        </form>
+<img class="class" src="{{asset('images/login4.jpg')}}">
+<div class="login-form">
+    <form action="{{route('login')}}" method="post">
+        {!! csrf_field() !!}
 
-    </div>
-    <div class="sub-cont">
-        <div class="img">
-            <div class="img__text m--up">
-                <h2>New here?</h2>
-                <p>Sign up and discover great amount of new opportunities!</p>
-            </div>
-            <div class="img__text m--in">
-                <h2>One of us?</h2>
-                <p>If you already has an account, just sign in. We've missed you!</p>
-            </div>
-            <div class="img__btn">
-                <span class="m--up">Sign Up</span>
-                <span class="m--in">Sign In</span>
-            </div>
+        <div class="avatar"><i class="fa fa-user-circle"></i></div>
+        <h4 class="modal-title">Login to Your Account</h4>
+        <div class="form-group">
+            <input type="text" name="email" class="form-control" placeholder="Email" required="required">
         </div>
-        <div class="form sign-up">
-            <h2>Time to feel like home,</h2>
-            <label>
-                <span>Name</span>
-                <input type="text"/>
-            </label>
-            <label>
-                <span>Email</span>
-                <input type="email"/>
-            </label>
-            <label>
-                <span>Password</span>
-                <input type="password"/>
-            </label>
-            <button type="button" class="submit">Sign Up</button>
-            <button type="button" class="fb-btn">Join with <span>facebook</span></button>
+        <div class="form-group">
+            <input name="password" type="password" class="form-control" placeholder="Password" required="required">
         </div>
-    </div>
+        <div class="form-group small clearfix">
+            <label class="checkbox-inline"><input type="checkbox"> Remember me</label>
+            <a href="#" class="forgot-link">Forgot Password?</a>
+        </div>
+        <input type="submit" class="btn btn-block btn-lg" value="Login">
+        <div class="text-center small">Don't have an account? <a href="#">Sign up</a></div>
+
+    </form>
 </div>
 </body>
-<script>
-    document.querySelector('.img__btn').addEventListener('click', function () {
-        document.querySelector('.cont').classList.toggle('s--signup');
-    });
-</script>
 </html>
